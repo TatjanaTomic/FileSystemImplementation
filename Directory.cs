05e443d7d8f49f063f8bcde8df670546a504d0bb
TatjanaTomic <34337951+TatjanaTomic@users.noreply.github.com>
2019-09-16 15:20:31 +0200

    Add files via upload

---
+++ Directory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemImplementation
+{ 
+    class Directory
+    {
+        public string DirectoryName { get; private set; }
+        public int DirectoryId { get; private set; }
+        public string DirectoryPath { get; private set; }
+        public DateTime DateCreated { get; private set; }
+
+        public Directory(string name, int id,  DateTime dateC, string path = "root/")
+        {
+            DirectoryName = name; 
+            DirectoryId = id;
+            DirectoryPath = path + DirectoryName;
+            DateCreated = dateC;
+        }
+
+        /// <summary>
+        /// Funkcija upisuje u FileSystem.bin datoteku podatke o kreiranom direktorijumu u vidu jednog zapisa u MFT tabeli
+        /// MFT = Master File Table
+        /// Prepisuje prvu liniju binarne datoteke, zapim upisuje novi zapis, potom prepisuje ostatak sadrzaja datoteke
+        /// </summary>
+        /// <returns>Funkcija vraca true/false u zavisnosti od toga da li je bilo moguće upisati podatke u datoteku</returns>
+        internal bool WriteToMFT()
+        {
+            byte[] content = File.ReadAllBytes("FileSystem.bin");
+
+            string mftRecord = "dir~" + DirectoryId.ToString()
+                                + "~" + DirectoryName
+                                + "~" + DirectoryPath
+                                + "~" + DateCreated.ToString() + "~";
+
+            if (content.Length + mftRecord.Length + 1 > FileSystem.freeSpace)
+            {
+                Console.WriteLine("Greska - nije moguce dodati direktorijum jer je memorija fajl sistema popunjena.");
+                return false;
+            }
+
+            BinaryWriter writer1 = new BinaryWriter(new FileStream("FileSystem.bin", FileMode.Truncate));
+            for (int i = 0; i < 51; i++) //prvih 50 bajta je rezervisano za prvu liniju
+                writer1.Write(content[i]);
+            writer1.Close();
+
+            StreamWriter writer2 = new StreamWriter(new FileStream("FileSystem.bin", FileMode.Append));
+            writer2.Write(mftRecord + '\n');
+            writer2.Close();
+
+            BinaryWriter writer3 = new BinaryWriter(new FileStream("FileSystem.bin", FileMode.Append));
+            for (int i = 51; i < content.Length; i++)
+                writer3.Write(content[i]);
+            writer3.Close();
+
+            return true;
+        }
+    }
+}
---
+++ FileOnFS.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemImplementation
+{
+    class FileOnFS
+    {
+        public string FileName { get; private set; }
+        public int FileId { get; private set; }
+        public string FilePath { get; private set; }
+        public DateTime DateCreated { get; private set; }
+        public int InitialSize { get; private set; }
+        public int NumberOfBlocks { get; private set; }
+
+        public FileOnFS(string name, int id, DateTime dateC, string path = "root/", int size = 0, int br = 0)
+        {
+            FileName = name;
+            FileId = id;
+            FilePath = path + name;
+            DateCreated = dateC;
+            InitialSize = size;
+            NumberOfBlocks = br;
+        }
+
+        /// <summary>
+        /// Funkcija upisuje u FileSystem.bin datoteku podatke o kreiranom fajlu u vidu jednog zapisa u MFT tabeli
+        /// MFT = Master File Table
+        /// Prepisuje prvu liniju binarne datoteke, zapim upisuje novi zapis, potom prepisuje ostatak sadrzaja datoteke
+        /// </summary>
+        /// <returns>Funkcija vraca true/false u zavisnosti od toga da li je bilo moguće upisati podatke u datoteku</returns>
+        internal bool WriteToMFT()
+        {
+            byte[] content = File.ReadAllBytes("FileSystem.bin");
+
+            string mftRecord = "file~" + FileId.ToString()
+                                 + "~" + FileName
+                                 + "~" + FilePath
+                                 + "~" + DateCreated.ToString()
+                                 + "~" + InitialSize.ToString()
+                                 + "~" + NumberOfBlocks.ToString();
+
+            if (content.Length + mftRecord.Length + 1 > FileSystem.freeSpace)
+            {
+                Console.WriteLine("Greska - nije moguce dodati datoteku jer je memorija fajl sistema popunjena.");
+                return false;
+            }
+
+            BinaryWriter writer1 = new BinaryWriter(new FileStream("FileSystem.bin", FileMode.Truncate));
+            for (int i = 0; i < 51; i++)
+                writer1.Write(content[i]);
+            writer1.Close();
+
+            StreamWriter writer2 = new StreamWriter(new FileStream("FileSystem.bin", FileMode.Append));
+            writer2.Write(mftRecord + '\n');
+            writer2.Close();
+
+            BinaryWriter writer3 = new BinaryWriter(new FileStream("FileSystem.bin", FileMode.Append));
+            for (int i = 51; i < content.Length; i++)
+                writer3.Write(content[i]);
+            writer3.Close();
+
+            return true;
+        }
+    }
+}
